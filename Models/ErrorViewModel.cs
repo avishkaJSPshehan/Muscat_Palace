3da7a99672d1e9ff25f25849a437abdaf1fad624
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-05-12 11:58:50 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Muscat_Palace.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
